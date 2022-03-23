dfd3bdf9ce7f19a7a1f3e0bc553b99fec36be0e2
NguyenQuangHiep111 <68044073+NguyenQuangHiep111@users.noreply.github.com>
2022-03-23 10:36:46 +0700

    lí thuyết buoi 1 java core

---
+++ day_01/hs/day_01.java
@@ -0,0 +1,53 @@
+package day_01.hs;
+
+public class day_01 {
+    public int age;
+    public static final double PI = 3.14;
+    public static void main(String[] args) {
+
+
+
+//	// write your code here
+//        System.out.println("hello");
+//        System.out.println("xin chào");
+//
+////        Scanner sc = new Scanner();
+//        int age ;// Khai báo biến
+//        age = 26;
+//        System.out.println(age);
+//
+//        long salary = 10000000L;
+//        System.out.println("Lương của tôi là :"+ salary);
+//
+//        float weight = 68.5f;
+//        double height = 1.7;
+//
+//        char c='c';
+//        char number = '5';
+
+
+//        double heght = 1.5;
+//        double weight = 40;
+//
+//        double BMI = weight/(heght*heght);
+//
+//        System.out.println("BMI : " + BMI);
+
+            double toan = 9;
+            double li = 8;
+            double hoa = 7;
+
+            double TB = (toan+li+hoa)/3;
+
+            System.out.println("TB : " + TB);
+            System.out.println("số pi :"+PI);
+            System.out.println("Xin chào \t tôi tên \n là \"Hiệp\"");
+
+
+            int a=5;
+            int b=2;
+
+            double c=(double) 5/2;
+            System.out.println(c);
+    }
+}
