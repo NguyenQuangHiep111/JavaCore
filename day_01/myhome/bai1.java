ebba1609cc956c8e68010c51e179b9a23d79f186
NguyenQuangHiep111 <68044073+NguyenQuangHiep111@users.noreply.github.com>
2022-03-23 15:56:44 +0700

    bài tập về nhà javacore tiếp theo

--- day_01/myhome/bai1.java
+++ day_01/myhome/bai1.java
@@ -1,6 +1,6 @@
 package day_01.myhome;
 
-public class baitapbuoi1 {
+public class bai1 {
     public static void main(String[] args) {
         int a = a=10,b=20;
         int p1 = a+b;
@@ -11,9 +11,10 @@ public class baitapbuoi1 {
         System.out.println("Kết quả b-a là :"+p3);
         int p4 = a*b;
         System.out.println("Kết quả a*b là :"+p4);
-//        int p5 = a/b;
-        double p5 =(double) a/b;
+        int p5 = a/b;
         System.out.println("Kết quả a/b là :"+p5);
+//        double p5 =(double) a/b;
+//        System.out.println("Kết quả a/b khi ép kiểu là :"+p5);
         int p6 = b/a;
         System.out.println("Kết quả b/a là :"+p6);
 
@@ -23,5 +24,10 @@ public class baitapbuoi1 {
 //        a*b
 //        a/b
 //        b/a
+
+
+
+
+
     }
 }
---
+++ day_01/myhome/bai2.java
@@ -0,0 +1,11 @@
+package day_01.myhome;
+
+public class bai2 {
+    public static void main(String[] args) {
+        int i = 20;
+        System.out.println("i++ có giá trị: "+ (i++));
+//        i++: Tăng giá trị i lên 1 nhưng trả về giá trị cũ i.
+        System.out.println("++i có giá trị: "+ (++i));
+//        ++i: tăng giá trị i lên 1 và trả về giá trị mới i + 1.
+    }
+}
