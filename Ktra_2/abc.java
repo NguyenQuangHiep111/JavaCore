3e3f91a9bfc4b988cf6486c9c1ad14e21d0ccbea
NguyenQuangHiep111 <68044073+NguyenQuangHiep111@users.noreply.github.com>
2022-04-21 20:06:05 +0700

    Ktra2tt

--- Ktra_2/abc.java
+++
@@ -1,6 +0,0 @@
-package Ktra_2;
-
-public class abc {
-//    listAcc.add(new Account("NgManhCuong248", "MCuong248001@123", "mcuong248@gmail.com"));
-//        listAcc.add(new Account("Mcuong2482001", "NgManhCuong2001@", "cuong2482001@gmail.com"));
-}
