67ae49463aa986677c1b1e926cc5258a950bcc25
NguyenQuangHiep111 <68044073+NguyenQuangHiep111@users.noreply.github.com>
2022-04-21 20:01:15 +0700

    Ktra2tt

--- Ktra_2/Validate.java
+++ Ktra_2/Validate.java
@@ -3,7 +3,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Validate {
     public static String EMAIL_REGEX = "^(.+)@(\\S+)$";
-    public static String PASSWORD_REGEX = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
+    public static String PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[\\.\\,\\(\\_]).{7,15}$\n";
     public static String USERNAME_REGEX = "^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$";
     public static boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_REGEX);
