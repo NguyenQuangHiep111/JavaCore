1b9cafd165e836980fbe2ea9b4ccd59e367b4264
NguyenQuangHiep111 <68044073+NguyenQuangHiep111@users.noreply.github.com>
2022-04-28 19:25:41 +0700

    28/4

---
+++ movie_exerice/src/main/java/model/Movie.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.Date;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String[] category;
+    private String direction;
+    private int length;
+    private Date release;
+    private long view;
+
+}
